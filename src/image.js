1d60fe2622dc1f401dceebdfd0589342789ff6ea
Dinika Saxena <dinikasaxenas@gmail.com>
2023-07-24 18:42:43 +0200

    Save annotated image

--- src/image.js
+++ src/image.js
@@ -1,7 +1,11 @@
+import { save } from "@tauri-apps/api/dialog";
+import { downloadDir } from '@tauri-apps/api/path';
+import { writeBinaryFile } from '@tauri-apps/api/fs';
+
 const canvas = document.getElementById("drawPlace");
 const context = canvas.getContext("2d");
 
-export function onFileUploaded(event) {
+export function onImageUpload(event) {
     if (event.target.files) {
         let file = event.target.files[0];
         var reader = new FileReader();
@@ -17,3 +21,26 @@ export function onFileUploaded(event) {
         }
     }
 }
+
+export function onImageDownload() {
+    var link = document.createElement('a');
+    link.download = 'filename.png';
+    const dataUrl = canvas.toDataURL();
+    link.href = dataUrl;
+    link.click();
+    saveImage(dataUrl);
+}
+
+const saveImage = async (dataUrl) => {
+    const suggestedFilename = "image.png";
+
+    // Save into the default downloads directory, like in the browser
+    const filePath = await save({
+        defaultPath: (await downloadDir()) + "/" + suggestedFilename,
+    });
+
+    const blob = await fetch(dataUrl).then((res) => res.blob());
+
+    const contents = await blob.arrayBuffer();
+    await writeBinaryFile(filePath, contents);
+};
